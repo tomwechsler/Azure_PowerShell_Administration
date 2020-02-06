8f57231bbbd860b8c1c5a8094b7505c7c31bee6c
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-02-06 09:00:27 +0100

    Add files via upload

---
+++ 16_Add_Policy_RG.ps1
@@ -0,0 +1,31 @@
+﻿Set-Location C:\
+Clear-Host
+
+Install-Module -Name Az -AllowClobber -Scope AllUsers
+
+# https://docs.microsoft.com/en-us/dotnet/framework/install/on-windows-10
+
+Import-Module Az
+
+# Connect to Azure with a browser sign in token
+Connect-AzAccount
+
+# Register the resource provider if it's not already registered
+Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'
+
+# Get a reference to the resource group that will be the scope of the assignment
+$rg = Get-AzResourceGroup -Name 'tw-web-rg'
+
+# Get a reference to the built-in policy definition that will be assigned
+$definition = Get-AzPolicyDefinition | Where-Object { $_.Properties.DisplayName -eq 'Audit VMs that do not use managed disks' }
+
+# Create the policy assignment with the built-in definition against your resource group
+New-AzPolicyAssignment -Name 'audit-vm-manageddisks' -DisplayName 'Audit VMs without managed disks Assignment' -Scope $rg.ResourceId -PolicyDefinition $definition
+
+# Get the resources in your resource group that are non-compliant to the policy assignment
+Get-AzPolicyState -ResourceGroupName $rg.ResourceGroupName -PolicyAssignmentName 'audit-vm-manageddisks' -Filter 'IsCompliant eq false'
+
+Get-AzSubscription
+
+# Removes the policy assignment
+Remove-AzPolicyAssignment -Name 'audit-vm-manageddisks' -Scope '/subscriptions/b8e8ea82-7d28-4438-b50e-fbcf18d04346/resourceGroups/tw-web-rg'
