512b4c5428cfd72390caf9f95e609c26084fd539
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-05-13 13:07:24 +0200

    Add files via upload

---
+++ Collect_details_about_all_VMs_in_subscription.ps1
@@ -0,0 +1,42 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+#Provide the name of the csv file to be exported
+$reportName = "myReport.csv"
+
+Select-AzSubscription $subscriptionId
+$report = @()
+$vms = Get-AzVM
+$publicIps = Get-AzPublicIpAddress 
+$nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
+foreach ($nic in $nics) { 
+    $info = "" | Select VmName, ResourceGroupName, Region, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+    $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+    foreach($publicIp in $publicIps) { 
+        if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
+            $info.PublicIPAddress = $publicIp.ipaddress
+            } 
+        } 
+        $info.OsType = $vm.StorageProfile.OsDisk.OsType 
+        $info.VMName = $vm.Name 
+        $info.ResourceGroupName = $vm.ResourceGroupName 
+        $info.Region = $vm.Location 
+        $info.VirturalNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3] 
+        $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1] 
+        $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress 
+        $report+=$info 
+    } 
+$report | ft VmName, ResourceGroupName, Region, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+$report | Export-CSV "$home/$reportName"
+
