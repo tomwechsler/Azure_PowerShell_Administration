da08cc1a147252766da5f7abed14705d94cc439a
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-06-02 14:50:40 +0200

    Add files via upload

---
+++ Scale_web_app_manually.ps1
@@ -0,0 +1,33 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+# Generates a Random Value
+$Random=(New-Guid).ToString().Substring(0,8)
+
+# Variables
+$ResourceGroupName="myResourceGroup$random"
+$AppName="AppServiceManualScale$random"
+$Location="WestEurope"
+
+# Create a Resource Group
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Create an App Service Plan
+New-AzAppservicePlan -Name AppServiceManualScalePlan -ResourceGroupName $ResourceGroupName -Location $Location -Tier Basic
+
+# Create a Web App in the App Service Plan
+New-AzWebApp -Name $AppName -ResourceGroupName $ResourceGroupName -Location $Location -AppServicePlan AppServiceManualScalePlan
+
+# Scale Web App to 2 Workers
+Set-AzAppServicePlan -NumberofWorkers 2 -Name AppServiceManualScalePlan -ResourceGroupName $ResourceGroupName
+
+#Clean Up
+Remove-AzResourceGroup -Name $ResourceGroupName -Force
