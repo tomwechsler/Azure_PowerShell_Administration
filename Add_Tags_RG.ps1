e0a7c7400e2e427e69c95b21859a9496dedf8425
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-07-17 15:37:37 +0200

    Add files via upload

---
+++ Add_Tags_RG.ps1
@@ -0,0 +1,28 @@
+﻿Set-Location C:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+#Tags
+(Get-AzResourceGroup -Name tw-rg01).Tags
+
+(Get-AzResource -ResourceName tw-winsrv -ResourceGroupName tw-rg01).Tags
+
+Set-AzResourceGroup -Name tw-rg01 -Tag @{ costcenter="1987"; ManagedBy="Bob" }
+
+$tags = (Get-AzResourceGroup -Name tw-rg01).Tags
+$tags.Add("Status", "Approved")
+Set-AzResourceGroup -Tag $tags -Name tw-rg01
+
+$r = Get-AzResource -ResourceName tw-winsrv -ResourceGroupName tw-rg01
+Set-AzResource -Tag @{ Dept="IT"; Environment="Test" } -ResourceId $r.ResourceId -Force
+
+$r = Get-AzResource -ResourceName tw-winsrv -ResourceGroupName tw-rg01
+$r.Tags.Add("Status", "Approved")
+Set-AzResource -Tag $r.Tags -ResourceId $r.ResourceId -Force
