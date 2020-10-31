dcaf8124f8310b767b8af525a1e7a191e3019e86
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-10-31 16:09:42 +0100

    Add files via upload

---
+++ Create_Storage_Account_SAS_token.ps1
@@ -0,0 +1,49 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+Get-AzContext
+
+#Variables
+$location = "westeurope"
+$rgname = "twstoragedemo"
+
+#A file we use later
+$today = Get-Date
+New-Item -ItemType file -Path C:\Temp\test.txt -Force -value $today
+
+#Create a Resource Group
+New-AzResourceGroup -Name $rgname -Location $location
+
+#Create a Storage Account
+New-AzStorageAccount -Location $location -ResourceGroupName $rgname -Name twstorage75 -SkuName Standard_LRS
+
+#We need at least one Storage Account Key
+$keys = Get-AzStorageAccountKey -Name twstorage75 -ResourceGroupName $rgname
+
+#Now we need to create Storage context
+$context = New-AzStorageContext -StorageAccountName twstorage75 -StorageAccountKey $keys[0].Value
+
+#Once we have it, let’s create a storage container
+New-AzStorageContainer -Context $context -Name bilder
+
+#Now we have required pre-requisites to create an SAS
+$token = New-AzStorageContainerSASToken -Context $context -Name bilder -Permission rwd
+
+#Now we need to create Storage Container context
+$containercontext = New-AzStorageContext -SasToken $token -StorageAccountName twstorage75
+
+#Let's upload a file to the Storage Container
+Set-AzStorageBlobContent -Context $containercontext -Container bilder -File C:\Temp\test.txt
+
+#List the blobs in the container
+Get-AzStorageBlob -Container bilder -Context $context | select Name, Blobtype, LastModified
+
+#Clean Up
+Remove-AzResourceGroup -Name $rgname -Force
