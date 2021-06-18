bbe5ed3f0382731b6259c7ea689eef448fe830e4
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-06-18 06:06:22 +0200

    Add files via upload

---
+++ Create_VM_Gallery.ps1
@@ -0,0 +1,85 @@
+﻿Set-Location c:\
+Clear-Host
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "Nutzungsbasierte Bezahlung" | Select-AzSubscription
+
+#Create some variables for the new VM.
+$resourceGroup = "myVMfromImage"
+$location = "WestEurope"
+$vmName = "myVMfromImage"
+
+#Get the image. Replace the name of your resource group, gallery, and image definition. This will create the VM from the latest image version available.
+$imageDefinition = Get-AzGalleryImageDefinition `
+   -GalleryName elmesig2021 `
+   -ResourceGroupName elme-SIG-rg `
+   -Name Win10Multi20H2
+
+#Create user object
+$cred = Get-Credential `
+   -Message "Enter a username and password for the virtual machine."
+
+#Create a resource group
+New-AzResourceGroup `
+   -Name $resourceGroup `
+   -Location $location
+
+#Network pieces
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+   -Name mySubnet `
+   -AddressPrefix 192.168.1.0/24
+$vnet = New-AzVirtualNetwork `
+   -ResourceGroupName $resourceGroup `
+   -Location $location `
+   -Name MYvNET `
+   -AddressPrefix 192.168.0.0/16 `
+   -Subnet $subnetConfig
+$pip = New-AzPublicIpAddress `
+   -ResourceGroupName $resourceGroup `
+   -Location $location `
+  -Name "mypublicdns$(Get-Random)" `
+  -AllocationMethod Static `
+  -IdleTimeoutInMinutes 4
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig `
+   -Name myNetworkSecurityGroupRuleRDP  `
+   -Protocol Tcp `
+  -Direction Inbound `
+   -Priority 1000 `
+   -SourceAddressPrefix * `
+   -SourcePortRange * `
+   -DestinationAddressPrefix * `
+   -DestinationPortRange 3389 `
+   -Access Allow
+$nsg = New-AzNetworkSecurityGroup `
+   -ResourceGroupName $resourceGroup `
+   -Location $location `
+  -Name myNetworkSecurityGroup `
+  -SecurityRules $nsgRuleRDP
+$nic = New-AzNetworkInterface `
+   -Name myNic `
+   -ResourceGroupName $resourceGroup `
+   -Location $location `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id `
+  -NetworkSecurityGroupId $nsg.Id
+
+#Create a virtual machine configuration using $imageDefinition.Id to use the latest image version.
+$vmConfig = New-AzVMConfig `
+   -VMName $vmName `
+   -VMSize Standard_D1_v2 | `
+   Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+   Set-AzVMSourceImage -Id $imageDefinition.Id | `
+   Add-AzVMNetworkInterface -Id $nic.Id
+
+#Create a virtual machine
+New-AzVM `
+   -ResourceGroupName $resourceGroup `
+   -Location $location `
+   -VM $vmConfig
