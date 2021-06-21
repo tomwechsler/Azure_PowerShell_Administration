4748dfe3b86cada92e85286392c7d102db3c5e80
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-06-21 06:28:00 +0200

    Add files via upload

---
+++ Secure_webserver_VM_Azure_TLS_SSL.ps1
@@ -0,0 +1,95 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+#Basic variables
+$resourceGroup = "myResourceGroupSecureWeb"
+$prefix = "tw"
+$location = "westeurope"
+$id = Get-Random -Minimum 1000 -Maximum 9999
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+
+#Create a resource group
+New-AzResourceGroup -ResourceGroupName $resourceGroup -Location $location
+
+#Create an Azure Key Vault
+$keyvaultName="$($prefix)keyvault$id"
+New-AzKeyVault -VaultName $keyvaultName `
+    -ResourceGroup $resourceGroup `
+    -Location $location `
+    -EnabledForDeployment
+
+#Generate a certificate and store in Key Vault
+$policy = New-AzKeyVaultCertificatePolicy `
+    -SubjectName "CN=www.tomwechsler.ch" `
+    -SecretContentType "application/x-pkcs12" `
+    -IssuerName Self `
+    -ValidityInMonths 12
+
+Add-AzKeyVaultCertificate `
+    -VaultName $keyvaultName `
+    -Name "mycert" `
+    -CertificatePolicy $policy
+
+#Set an administrator username and password for the VM
+$cred = Get-Credential
+
+# Create a VM
+New-AzVm `
+    -ResourceGroupName $resourceGroup `
+    -Name "myVM" `
+    -Location $location `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -Credential $cred `
+    -OpenPorts 443
+
+# Use the Custom Script Extension to install IIS
+Set-AzVMExtension -ResourceGroupName $resourceGroup `
+    -ExtensionName "IIS" `
+    -VMName "myVM" `
+    -Location $location `
+    -Publisher "Microsoft.Compute" `
+    -ExtensionType "CustomScriptExtension" `
+    -TypeHandlerVersion 1.8 `
+    -SettingString '{"commandToExecute":"powershell Install-WindowsFeature Web-Server -IncludeManagementTools"}'
+
+#Add a certificate to VM from Key Vault
+$certURL=(Get-AzKeyVaultSecret -VaultName $keyvaultName -Name "mycert").id
+
+$vm=Get-AzVM -ResourceGroupName $resourceGroup -Name "myVM"
+$vaultId=(Get-AzKeyVault -ResourceGroupName $resourceGroup -VaultName $keyVaultName).ResourceId
+$vm = Add-AzVMSecret -VM $vm -SourceVaultId $vaultId -CertificateStore "My" -CertificateUrl $certURL
+
+Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
+
+#Configure IIS to use the certificate
+$PublicSettings = '{
+    "fileUris":["https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/secure-iis.ps1"],
+    "commandToExecute":"powershell -ExecutionPolicy Unrestricted -File secure-iis.ps1"
+}'
+
+Set-AzVMExtension -ResourceGroupName $resourceGroup `
+    -ExtensionName "IIS" `
+    -VMName "myVM" `
+    -Location $location `
+    -Publisher "Microsoft.Compute" `
+    -ExtensionType "CustomScriptExtension" `
+    -TypeHandlerVersion 1.8 `
+    -SettingString $publicSettings
+
+#Test the secure web app
+Get-AzPublicIPAddress -ResourceGroupName $resourceGroup -Name "myPublicIPAddress" | select "IpAddress"
+
+#Now you can open a web browser and enter https://<myPublicIP>
+
+#Clean Up
+Remove-AzResourceGroup -Name "myResourceGroupSecureWeb" -Force
