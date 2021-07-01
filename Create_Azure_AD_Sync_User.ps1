3c99f4efe5f4460acd690f066f25351936621147
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-07-01 06:06:49 +0200

    Add files via upload

---
+++ Create_Azure_AD_Sync_User.ps1
@@ -0,0 +1,46 @@
+﻿Set-Location c:\
+Clear-Host
+
+#We need the cmdlets
+Install-Module -Name AzureAD -AllowClobber -Force -Verbose
+
+#Sometimes the module must be imported
+Import-Module AzureAD
+
+#Username and PW for Login
+$Credential = Get-Credential
+
+#Lets connect to the Azure Active Directory
+Connect-AzureAD -Credential $Credential
+
+#View all accounts
+Get-AzureADUser
+
+#Some varibales
+$userName = 'aadsyncuser'
+$aadDomainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+
+#Create password profile
+$passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$passwordProfile.Password = 'Pa55w.rd1234'
+$passwordProfile.ForceChangePasswordNextLogin = $false
+
+#Create the user
+New-AzureADUser -AccountEnabled $true -DisplayName $userName -PasswordProfile $passwordProfile -MailNickName $userName -UserPrincipalName "$userName@$aadDomainName"
+
+#Some more variables
+$aadUser = Get-AzureADUser -ObjectId "$userName@$aadDomainName"
+$aadRole = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq 'Global administrator'} 
+
+#Set the role
+Add-AzureADDirectoryRoleMember -ObjectId $aadRole.ObjectId -RefObjectId $aadUser.ObjectId
+
+#Identify the user principal name
+(Get-AzureADUser -Filter "MailNickName eq '$userName'").UserPrincipalName
+
+#Some more variables
+$groupObjectId = (Get-AzureADGroup -Filter "DisplayName eq 'AAD DC Administrators'").ObjectId
+$userObjectId = (Get-AzureADUser -Filter "MailNickName eq 'aadadmin1'").ObjectId
+
+#Add the user to a specific group (optional)
+Add-AzureADGroupMember -ObjectId $groupObjectId -RefObjectId $userObjectId
