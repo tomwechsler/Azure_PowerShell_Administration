1ce18ee6f8576e196329880ae9c8b7c27129a6dc
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-07-14 10:09:10 +0200

    Add files via upload

---
+++ Finding_User_with_AzureAD_Admin_Roles.ps1
@@ -0,0 +1,52 @@
+﻿Set-Location c:\
+Clear-Host
+
+#We need the cmdlets
+Install-Module -Name AzureAD -AllowClobber -Force -Verbose
+
+#Sometimes the module must be imported
+Import-Module AzureAD
+
+#Let's connect
+Connect-AzureAD
+
+#To explore the available cmdlets in the Azure AD module
+Get-Command -Module AzureAD | Measure-Object
+
+#Fetch list of all directory roles with object ID
+Get-AzureADDirectoryRole
+
+#Fetch a specific directory role by ID
+$role = Get-AzureADDirectoryRole -ObjectId "6fd5c3ac-2e62-4fca-84fe-9e32ae5282f2"
+
+#Fetch role membership for a role
+Get-AzureADDirectoryRoleMember -ObjectId $role.ObjectId | Get-AzureADUser
+
+#Lets create some variables
+$roleUsers = @() 
+$roles=Get-AzureADDirectoryRole
+
+#We use a loop  
+ForEach($role in $roles) {
+  $users=Get-AzureADDirectoryRoleMember -ObjectId $role.ObjectId
+  ForEach($user in $users) {
+    write-host $role.DisplayName,$user.DisplayName,$user.UsageLocation
+    $obj = New-Object PSCustomObject
+    $obj | Add-Member -type NoteProperty -name RoleName -value ""
+    $obj | Add-Member -type NoteProperty -name UserDisplayName -value ""
+    $obj | Add-Member -type NoteProperty -name UsageLocation -value ""
+    $obj.RoleName=$role.DisplayName
+    $obj.UserDisplayName=$user.DisplayName
+    $obj.UsageLocation=$user.UsageLocation
+    $roleUsers+=$obj
+  }
+}
+
+#We have a result
+$roleUsers
+
+#A bit more readable
+$roleUsers | Sort-Object Userdisplayname | select Userdisplayname, RoleName
+
+#Remove the session
+Disconnect-AzureAD
