792c8a6697bebe1e48ce85245d094a49e9cec6c0
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-12 06:25:24 +0200

    Add files via upload

---
+++ Inverstigate_Azure_App_Service_Principals.ps1
@@ -0,0 +1,67 @@
+﻿Set-Location c:\
+Clear-Host
+
+#We need the cmdlets
+Install-Module -Name AzureAD -AllowClobber -Force -Verbose
+
+#Sometimes the module must be imported
+Import-Module AzureAD
+
+#Let's connect
+Connect-AzureAD
+
+#Get a List of the apps
+Get-AzureADApplication
+
+#A bit more info
+Get-AzureADApplication -Filter "DisplayName eq 'twdemoapp'" | Format-List *
+
+#Let's create a variable
+$sp = Get-AzureADServicePrincipal -Filter "displayName eq 'twdemoapp'"
+$sp.ObjectId
+
+#Get Azure AD App role assignments using objectId of the Service Principal
+$assignments = Get-AzureADServiceAppRoleAssignment -ObjectId $sp.ObjectId -All $true
+
+#Remove all users and groups assigned to the application
+$assignments | ForEach-Object {
+    if ($_.PrincipalType -eq "User") {
+        Remove-AzureADUserAppRoleAssignment -ObjectId $_.PrincipalId -AppRoleAssignmentId $_.ObjectId
+    } elseif ($_.PrincipalType -eq "Group") {
+        Remove-AzureADGroupAppRoleAssignment -ObjectId $_.PrincipalId -AppRoleAssignmentId $_.ObjectId
+    }
+}
+
+#Get Azure AD App role assignments again
+$assignments = Get-AzureADServiceAppRoleAssignment -ObjectId $sp.ObjectId -All $true | Where-Object {$_.PrincipalType -eq "User"}
+
+#Let's check
+$assignments
+
+#Get all delegated permissions for the service principal
+$spOAuth2PermissionsGrants = Get-AzureADOAuth2PermissionGrant -All $true| Where-Object { $_.clientId -eq $sp.ObjectId }
+
+#Remove all delegated permissions
+$spOAuth2PermissionsGrants | ForEach-Object {
+    Remove-AzureADOAuth2PermissionGrant -ObjectId $_.ObjectId
+}
+
+#Get all delegated permissions again
+$spOAuth2PermissionsGrants = Get-AzureADOAuth2PermissionGrant -All $true| Where-Object { $_.clientId -eq $sp.ObjectId }
+
+#Let's check
+$spOAuth2PermissionsGrants
+
+#Get all application permissions for the service principal
+$spApplicationPermissions = Get-AzureADServiceAppRoleAssignedTo -ObjectId $sp.ObjectId -All $true | Where-Object { $_.PrincipalType -eq "ServicePrincipal" }
+
+#Remove all delegated permissions
+$spApplicationPermissions | ForEach-Object {
+    Remove-AzureADServiceAppRoleAssignment -ObjectId $_.PrincipalId -AppRoleAssignmentId $_.objectId
+}
+
+#Get all application permissions again
+$spApplicationPermissions = Get-AzureADServiceAppRoleAssignedTo -ObjectId $sp.ObjectId -All $true | Where-Object { $_.PrincipalType -eq "ServicePrincipal" }
+
+#Let's check
+$spApplicationPermissions
