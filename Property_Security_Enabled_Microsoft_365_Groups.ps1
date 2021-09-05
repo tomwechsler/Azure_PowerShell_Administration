118fa7d0257bcc471c3ce08fc9e57c0d09d97367
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-09-05 07:18:20 +0200

    Add files via upload

---
+++ Property_Security_Enabled_Microsoft_365_Groups.ps1
@@ -0,0 +1,43 @@
+﻿Set-Location c:\
+Clear-Host
+
+#If needed
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+#Install the AzureAD
+Install-Module -Name AzureAD -AllowClobber -Verbose -Force
+
+#Connect
+Connect-AzureAD
+
+#Did it work?
+Get-AzureADUser
+
+#Create a dynamic group
+New-AzureADMSGroup -DisplayName "Dynamic Group 01" -Description "Dynamic group created from PS" -MailEnabled $False -MailNickName "group" -SecurityEnabled $True -GroupTypes "DynamicMembership" -MembershipRule "(user.department -contains ""Marketing"")" -MembershipRuleProcessingState "On"
+
+#Get the group
+Get-AzureADMSGroup -SearchString "Dynamic Group 01"
+
+#The value true comes back
+(Get-AzureADMSGroup -SearchString "Dynamic Group 01").SecurityEnabled
+
+#Get an existing group
+Get-AzureADMSGroup -SearchString "Technik"
+
+#Get the SecurityEnabled value
+(Get-AzureADMSGroup -SearchString "Technik").SecurityEnabled
+
+#Now let's have a look at the Azure Active Directory Portal
+#Under Manage we do not find a license option!
+
+#We change the SecurityEnabled property
+Set-AzureADMSGroup -Id a8269c21-1059-4bb1-8937-7f2d6a6f6b92 -SecurityEnabled $True
+
+#Let's check it out in Azure Active Directory!
+
+#When you create a new Microsoft 365 group in the Microsoft 365 portal, you cannot work with group licensing 
+#in Azure Active Directory because the group's SecurityEnabled property has the value false.
+
+#If you create a new Microsoft 365 group in Azure Active Directory, you can work with group licensing in 
+#Azure AD because the SecurityEnabled property has a value of true.
