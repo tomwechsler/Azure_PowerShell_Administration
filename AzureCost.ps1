81fc6af8c0dbb9ab956955e65e31ad0c2b2a6c09
Tom Wechsler [Microsoft Azure & CDM MVP] <56835948+tomwechsler@users.noreply.github.com>
2024-02-10 14:35:18 +0100

    Create AzureCost.ps1

---
+++ AzureCost.ps1
@@ -0,0 +1,43 @@
+#Install the Az.Billing module if you have not already done so
+Install-Module -Name Az.Billing -AllowClobber -Verbose -Force
+
+#Sign in to your Azure account
+Connect-AzAccount
+
+#List all subscriptions
+Get-AzSubscription 
+
+#Select your subscription
+$subscriptionId = "b3e430d7-6706-40c1-a350-a9f9deff9326"
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+#Define the period for which you want to retrieve the cost information
+$startDate = (Get-Date).AddMonths(-1)
+$endDate = Get-Date
+
+#Retrieve the cost information
+$costs = Get-AzConsumptionUsageDetail -StartDate $startDate -EndDate $endDate
+
+#Sort the cost information by total cost and display the most expensive resources first
+$sortedCosts = $costs | Sort-Object -Property PreTaxCost -Descending
+
+#Output of the cost information
+$sortedCosts | Format-Table -Property InstanceName, PreTaxCost
+
+#A little more precise - The costs added up
+
+#Calculate the sum of the PreTaxCost for each MeterId
+$costSummary = $costs | Group-Object -Property MeterId | ForEach-Object {
+    $totalCost = ($_.Group | Measure-Object -Property PreTaxCost -Sum).Sum
+    [PSCustomObject]@{
+        MeterId = $_.Name
+        InstanceName = $_.Group[0].InstanceName
+        TotalPreTaxCost = $totalCost
+    }
+}
+
+#Sort the cost information by total cost and display the most expensive resources first
+$sortedCostSummary = $costSummary | Sort-Object -Property TotalPreTaxCost -Descending
+
+#Output of the cost information
+$sortedCostSummary | Format-Table -Property MeterId, InstanceName, TotalPreTaxCost
